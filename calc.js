664d380373a95e74719df5c7957c125aca6e9033
shivaniyadawar <yadawarshivani@gmail.com>
2023-10-12 23:13:06 +0530

    Added more functionality relted to input.

--- calc.js
+++ calc.js
@@ -1,6 +1,6 @@
 
     var val = document.getElementById("rBox");
-
+    var letters = /^([0-9]+(\.[0-9]+)?|[-+*/])+$/;
     function addInp(input){
         val.value += input;
     }
@@ -12,3 +12,23 @@
     function cls(){
         val.value=""
     }
+
+    document.addEventListener("keydown", (event) => {
+        if(event.key == "="){
+            if(val.value.match(letters)){
+                equ();
+            }else{
+                 alert("invalid input");
+            }
+            
+        }else if(event.key == "Backspace"){
+                val.value = val.value.slice(0,-1);}
+
+        else if(event.key == "Shift"|| event.key =="Control" || event.key =="Delete"){
+            val.value = val.value.replace(/[^0-9]+/,"");
+        }
+        else{
+                val.value += event.key;
+            }
+        
+    })
